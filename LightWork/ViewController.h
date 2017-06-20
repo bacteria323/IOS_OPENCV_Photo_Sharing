1d29d82d04fa19ef6fbb241fe019f01eddf9274c
Shun Lee <shunlee94@hotmail.com>
2017-06-20 13:15:41 +0800

    initial project setup

---
+++ LightWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightWork
+//
+//  Created by Shun Lee on 20/6/2017.
+//  Copyright © 2017 mustardLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LightWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LightWork
+//
+//  Created by Shun Lee on 20/6/2017.
+//  Copyright © 2017 mustardLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
