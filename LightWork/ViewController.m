7126b47fad30ef73707bd92564428f1d9aaeb693
Shun Lee <shunlee94@hotmail.com>
2017-06-22 12:48:04 +0800

    add dummy full screen image and toolbar at the bottom with switch and 2 buttons

--- LightWork/ViewController.m
+++ LightWork/ViewController.m
@@ -6,9 +6,52 @@
 //  Copyright © 2017 mustardLabs. All rights reserved.
 //
 
+#import <Photos/Photos.h>
+#import <Social/Social.h>
+
+#import <opencv2/core.hpp>
+#import <opencv2/imgcodecs.hpp>
+#import <opencv2/imgcodecs/ios.h>
+#import <opencv2/imgproc.hpp>
+
+#import "ViewController.h"
+#import "VideoCamera.h"
+
 #import "ViewController.h"
 
-@interface ViewController ()
+//@interface ViewController () <CvVideoCameraDelegate> {
+@interface ViewController () {
+    cv::Mat originalStillMat;
+    cv::Mat updatedStillMatGray;
+    cv::Mat updatedStillMatRGBA;
+    cv::Mat updatedVideoMatGray;
+    cv::Mat updatedVideoMatRGBA;
+}
+
+@property IBOutlet UIImageView *imageView;
+@property IBOutlet UIActivityIndicatorView *activityIndicatorView;
+@property IBOutlet UIToolbar *toolbar;
+
+@property VideoCamera *videoCamera;
+@property BOOL saveNextFrame;
+
+//- (IBAction)onTapToSetPointOfInterest:
+//(UITapGestureRecognizer *)tapGesture;
+//- (IBAction)onColorModeSelected:
+//(UISegmentedControl *)segmentedControl;
+//- (IBAction)onSwitchCameraButtonPressed;
+//- (IBAction)onSaveButtonPressed;
+
+//- (void)refresh;
+//- (void)processImage:(cv::Mat &)mat;
+//- (void)processImageHelper:(cv::Mat &)mat;
+//- (void)saveImage:(UIImage *)image;
+//- (void)showSaveImageFailureAlertWithMessage:(NSString *)message;
+//- (void)showSaveImageSuccessAlertWithImage:(UIImage *)image;
+//- (UIAlertAction *)shareImageActionWithTitle:(NSString *)title
+//                                 serviceType:(NSString *)serviceType image:(UIImage *)image;
+//- (void)startBusyMode;
+//- (void)stopBusyMode;
 
 @end
 
@@ -17,7 +60,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    UIImage *originalStillImage = [UIImage imageNamed:@"Fleur.jpg"];
+//    UIImageToMat(originalStillImage, originalStillMat);
+//    
+//    self.videoCamera = [[VideoCamera alloc] initWithParentView:self.imageView];
+//    self.videoCamera.delegate = self;
+//    self.videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPresetHigh;
+//    self.videoCamera.defaultFPS = 30;
+//    self.videoCamera.letterboxPreview = YES;
 }
+
+//- (void)viewDidLayoutSubviews {
+//    [super viewDidLayoutSubviews];
+//    
+//    switch ([UIDevice currentDevice].orientation) {
+//        case UIDeviceOrientationPortraitUpsideDown:
+//            self.videoCamera.defaultAVCaptureVideoOrientation =
+//            AVCaptureVideoOrientationPortraitUpsideDown;
+//            break;
+//        case UIDeviceOrientationLandscapeLeft:
+//            self.videoCamera.defaultAVCaptureVideoOrientation =
+//            AVCaptureVideoOrientationLandscapeLeft;
+//            break;
+//        case UIDeviceOrientationLandscapeRight:
+//            self.videoCamera.defaultAVCaptureVideoOrientation =
+//            AVCaptureVideoOrientationLandscapeRight;
+//            break;
+//        default:
+//            self.videoCamera.defaultAVCaptureVideoOrientation =
+//            AVCaptureVideoOrientationPortrait;
+//            break;
+//    }
+//    
+//    [self refresh];
+//}
+//
+//- (IBAction)onTapToSetPointOfInterest:
+//(UITapGestureRecognizer *)tapGesture {
+//    if (tapGesture.state == UIGestureRecognizerStateEnded) {
+//        if (self.videoCamera.running) {
+//            CGPoint tapPoint =
+//            [tapGesture locationInView:self.imageView];
+//            [self.videoCamera
+//             setPointOfInterestInParentViewSpace:tapPoint];
+//        }
+//    }
+//}
 
 
 - (void)didReceiveMemoryWarning {
